a5b9ed5af062745771931b82ce3a3ade31c8c84d
Marina Cerame <marina.cerame@gmail.com>
2022-05-27 16:53:43 -0500

    [Console] Add check to ensure console app is open before trying to change scroll height

--- wfh/programs/console.php
+++ wfh/programs/console.php
@@ -1,34 +1,48 @@
 <div class="explorer noselect">
     <div class="explorer-space">
-      <div class="status">
-		  <div class="tab active"><i class="material-icons foldericon">Terminal</i></div>
-        <div class="bar-button"><i class="material-icons close-icon" id='close'>clear</i></div>
-      </div>
-		<div id='console'>
-		<img src='../../../assets/imgs/loading.gif' class='loading'  width='54px' height='54px'>
-		</div>
-		</div>
-	</div>
+        <div class="status">
+            <div class="tab active"><i class="material-icons foldericon">Terminal</i></div>
+            <div class="bar-button"><i class="material-icons close-icon" id='close'>clear</i></div>
+        </div>
+        <div id='console'>
+            <img src='../../../assets/imgs/loading.gif' class='loading' width='54px' height='54px'>
+        </div>
+    </div>
+</div>
 <script>
-   $(document).ready(function () {
-                function loadLog() {
-                    var oldscrollHeight = $("#console")[0].scrollHeight - 20; //Scroll height before the request
- 
-                    $.ajax({
-                        url: "log.php",
-                        cache: false,
-                        success: function (html) {
-                            $("#console").html(html); //Insert chat log into the #chatbox div
- 
-                            //Auto-scroll           
-                            var newscrollHeight = $("#console")[0].scrollHeight - 20; //Scroll height after the request
-                            if(newscrollHeight > oldscrollHeight){
-                                $("#console").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
-                            }   
-                        }
-                    });
+    $(document).ready(function() {
+        function loadLog() {
+            var consoleModal = $("#console")[0];
+
+            // Clear interval if console window has been closed.
+            if (!consoleModal) {
+                clearInterval(logCheck);
+                return;
+            }
+            var oldScrollHeight = consoleModal.scrollHeight - 20; //Scroll height before the request
+
+            $.ajax({
+                url: "log.php",
+                cache: false,
+                success: function(html) {
+                    var consoleModal = $("#console");
+                    if (!consoleModal) {
+                        return;
+                    }
+
+                    $("#console").html(html); //Insert chat log into the #chatbox div
+
+                    //Auto-scroll           
+                    var newScrollHeight = $("#console")[0].scrollHeight - 20; //Scroll height after the request
+                    if (newScrollHeight && newScrollHeight > oldScrollHeight) {
+                        $("#console").animate({
+                            scrollTop: newScrollHeight
+                        }, 'normal'); //Autoscroll to bottom of div
+                    }
                 }
- 
-                setInterval (loadLog, 2500);
             });
+        }
+
+        var logCheck = setInterval(loadLog, 2500);
+    });
 </script>
